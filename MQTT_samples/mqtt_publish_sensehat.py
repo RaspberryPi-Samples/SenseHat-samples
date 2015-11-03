ae10719e4cd8a81d3ee87656bfb391c3b84218f6
Lionel <lionel.walter@univ-poitiers.fr>
2015-11-03 15:02:29 +0100

    mise au point

--- MQTT_samples/mqtt_publish_sensehat.py
+++ MQTT_samples/mqtt_publish_sensehat.py
@@ -3,23 +3,33 @@
 import mosquitto
 from mqtt_settings import config
 import time
-from timestamp import now_timestamp
 #import numpy as np
 import json
 from sense_hat import SenseHat
 from collections import OrderedDict
 
-d_sensors = OrderedDict([
-    ('pixels', sense.get_pixels),
-    ('humidity', sense.get_humidity),
-    ('temperature', sense.get_temperature_from_humidity),
-    ('temperature_from_pressure', sense.get_temperature_from_pressure),
-    ('pressure', sense.get_pressure),
-    ('orientation', sense.get_orientation),
-    ('compass', sense.get_compass),
-    ('gyroscope', sense.get_gyroscope),
-    ('accelerometer', sense.get_accelerometer),
-])
+import datetime
+import pytz
+
+UNIX_EPOCH = datetime.datetime(1970, 1, 1, 0, 0) #offset-naive datetime
+TS_MULT_us = 1e6
+
+def now_timestamp(ts_mult=TS_MULT_us, epoch=UNIX_EPOCH):
+    return(int((datetime.datetime.utcnow() - epoch).total_seconds()*ts_mult))
+
+def all_sensors(sense):
+    d_sensors = OrderedDict([
+        ('pixels', sense.get_pixels),
+        ('humidity', sense.get_humidity),
+        ('temperature', sense.get_temperature_from_humidity),
+        ('temperature_from_pressure', sense.get_temperature_from_pressure),
+        ('pressure', sense.get_pressure),
+        ('orientation', sense.get_orientation),
+        ('compass', sense.get_compass),
+        ('gyroscope', sense.get_gyroscope),
+        ('accelerometer', sense.get_accelerometer),
+    ])
+    return d_sensors
 
 def on_message(mosq, obj, msg):
     print("%-20s %d %s" % (msg.topic, msg.qos, msg.payload))
@@ -42,34 +52,18 @@ def main():
     cli.connect(config['host'], config['port'], config['keepalive'])
     sense = SenseHat()
     sense.set_imu_config(True, True, True)
-    
-    #y = 100 # initial value
+    d_sensors = all_sensors(sense)
 
     while cli.loop() == 0:
-        now = now_timestamp()
-        #y = y + np.random.uniform(-1, 1)
-        #d_acc = sense.get_accelerometer_raw()
-        #humidity = sense.get_humidity()
-        #temp = sense.get_temperature()
-        #pressure = sense.get_pressure()
-        
-        #accelero = {
-        #    'now': now,
-        #    'data': d_acc
-        #    }
-            
-        #hum = {
-        #    'now' : now,
-        #    'data' : {
-        #        'y': humidity
-        #    }
-        #}
+        #now = now_timestamp()
+        now = datetime.datetime.now(pytz.utc)
         
         sensors = d_sensors.keys() # all sensors
+        #print(sensors)
 
         for sensor in sensors:
             get_value = d_sensors[sensor] # get a "callable"
-            print(sensor, get_value)
+            #print(sensor, get_value)
 
             data = {
                 'ts': now.isoformat(),
@@ -77,9 +71,10 @@ def main():
                     sensor: get_value()
                 }
             }
+            #print(data)
             
-        payload = json.dumps(data) # serialization
-        cli.publish(topic='/sensors/SenseHat01/%s' % sensor, payload=payload, qos=0, retain=False)
+            payload = json.dumps(data) # serialization
+            cli.publish(topic='/sensors/SenseHat01/%s' % sensor, payload=payload, qos=0, retain=False)
         
         #cli.publish(topic='/sensors/sensor01', payload=payload, qos=0, retain=False)
         #payload = json.dumps(hum) # serialization
--- MQTT_samples/mqtt_settings.py
+++ MQTT_samples/mqtt_settings.py
@@ -2,7 +2,7 @@
 
 config = {
     #'host': '192.168.100.105',
-    'host' : '192.168.1.1',
+    'host' : '127.0.0.1',
     'port': 1883,
     'keepalive': 60,
     'bind_address': ''
